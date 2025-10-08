8ef73d03662d6d09a0d41c824b24a2e76382a8f9
Ferdinand-MZ <ferdinandmaulanazafauzi@gmail.com>
2025-10-08 21:33:47 +0700

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,12 @@
-import Link from "next/link";
-import { Wrench } from "lucide-react";
-
 export function Footer() {
-  return (
-    <footer className="border-t py-6">
-      <div className="container flex flex-col items-center gap-4 px-4 md:px-6">
-        <p className="text-sm text-muted-foreground text-center">
-          © 2025 Aura 3.0 All rights reserved.
-        </p>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="w-full border-t py-6">
+            <div className="w-full max-w-screen-lg mx-auto flex flex-col items-center gap-4 px-4 md:px-6">
+                <p className="text-sm text-muted-foreground text-center">
+                2025 MindMate. All rights reserved.
+                </p>
+            </div>
+        </footer>
+
+    );
 }
